883a0e986701c5fe4989c9337f2f2640423a5f28
Om Bhise <bhiseom04@gmail.com>
2026-05-06 23:40:05 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,98 @@
+// import { use, useEffect, useState } from 'react';
+// import './App.css'
+// import axios from "axios";
+
+// function App() {
+  
+//   const [quotes, setQuotes] = useState([]);
+
+//   useEffect(() => {
+//     axios.get("http://localhost:5000/quotes")
+//     .then((res) => {
+//       setQuotes(res.data);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+
+//   }, [])
+
+//   const deleteQuote = (id) => {
+//     axios.delete(`http://localhost/quotes/${id}`)
+//     .then(() => {
+
+//     })
+//   }
+
+//   return (
+//     <>
+//      {quotes.map((q) => (
+//         <div key={q.id}>
+//           <p>{q.text}</p>
+//           <h4>{q.author}</h4>
+//           <button onClick={() => deleteQuote(q.id)}>
+//             Delete
+//           </button>
+//         </div>
+//      ))}
+//     </>
+//   )
+// }
+
+// export default App
+
+
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function App() {
+
+  const [quotes, setQuotes] = useState([]);
+
+  // GET
+  const fetchQuotes = () => {
+    axios.get("http://localhost:5000/quotes")
+      .then((res) => {
+        setQuotes(res.data);
+      });
+  };
+
+  useEffect(() => {
+    fetchQuotes();
+  }, []);
+
+  // DELETE
+  const deleteQuote = (id) => {
+
+    axios.delete(`http://localhost:5000/quotes/${id}`)
+      .then(() => {
+        fetchQuotes();
+      });
+
+  };
+
+  return (
+    <div>
+
+      <h1>Quotes</h1>
+
+      {quotes.map((q) => (
+        <div key={q.id}>
+
+          <p>{q.text}</p>
+          <h4>{q.author}</h4>
+
+          <button onClick={() => deleteQuote(q.id)}>
+            Delete
+          </button>
+
+          <hr />
+
+        </div>
+      ))}
+
+    </div>
+  );
+}
+
+export default App;
